526ce61749de2664fb209e6c6457163e65f8251b
Abhik Laha <abhiklaha2000@gmail.com>
2024-05-30 14:05:37 +0530

    db change

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/E-commerce_Backend").then(() => {
+mongoose.connect(`mongodb+srv://abhiklaha2000:dumdum123@cluster0.mae0k64.mongodb.net/E-commerce_Backend`).then(() => {
     console.log('Connected to Mongodb!!!');
 }
 ).catch((err) =>{
